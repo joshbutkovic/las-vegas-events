bcbfccf696a14fa7bef5a72a3b2accf48da08ba9
Josh Butkovic <joshbutkovic@gmail.com>
2019-05-26 08:20:30 -0700

    mapping Events to Event
    mapping Events to Event

--- src/components/Events/Event/Event.js
+++ src/components/Events/Event/Event.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-const Event = (...props) => {
-	return (
-		<div class="container is-widescreen">
-			<div class="notification">EVENT NAME: name</div>
-		</div>
-	);
+const Event = ({ ...props }) => {
+  // aggregate and filter event here
+
+  return (
+    <div class="container is-widescreen">
+      <div class="notification">
+        EVENT NAME: {props.event.name || "no event"}
+      </div>
+    </div>
+  );
 };
 
 export default Event;
--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Container } from "../../utils/animations";
 import { useHttpGet } from "../../hooks/http";
 import { GET_EVENTS_URL } from "../../config/http-config";
-import { Event } from "../Events/Event/Event";
+import Event from "../Events/Event/Event";
+import "../../components/Events/Events.css";
 
 const Events = props => {
   const [isLoading, returnedEvents] = useHttpGet(GET_EVENTS_URL, [
@@ -14,31 +15,36 @@ const Events = props => {
   let content = <p>Loading events...</p>;
 
   if (!isLoading && selectedEvents && selectedEvents.length > 0) {
-    content = "HI";
+    console.log(selectedEvents);
+    const eventComponents = selectedEvents.map(event => (
+      <div class="container is-widescreen event-container">
+        <div class="notification">
+          <Event event={event} />
+        </div>
+      </div>
+    ));
+
+    content = (
+      <Container>
+        <div className="container">
+          <section className="section">
+            <div className="columns">
+              <div className="column">
+                <h1 className="is-size-2 has-text-centered">
+                  LAS VEGAS EVENTS
+                </h1>
+                {eventComponents}
+              </div>
+            </div>
+          </section>
+        </div>
+      </Container>
+    );
+  } else if (!isLoading && (!selectedEvents || selectedEvents.length === 0)) {
+    content = <p>No Events returned</p>;
   }
 
-  // } else if (!isLoading && (!selectedEvents || selectedEvents.length === 0)) {
-  //   content = <p>Could not fetch any data.</p>;
-  // }
-
   return content;
-
-  // let eventsPageContent = (
-  //   <Container>
-  //     <div className="container">
-  //       <section className="section">
-  //         <div className="columns">
-  //           <div className="column">
-  //             <h1 className="is-size-2 has-text-centered">LAS VEGAS EVENTS</h1>
-  //             {eventContent}
-  //           </div>
-  //         </div>
-  //       </section>
-  //     </div>
-  //   </Container>
-  // );
-
-  // return eventsPageContent;
 };
 
 export default Events;
